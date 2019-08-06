16906caed30ab86c3d27c8e97da2235d6d28eb2a
goalfinance <theon_pan@outlook.com>
2019-08-06 15:00:28 +0800

    initial version

---
+++ Attendance/ImportAttendance.py
@@ -0,0 +1,34 @@
+from getopt import getopt
+from getopt import GetoptError
+import sys
+import os
+import openpyxl
+from goalfinace.utils.utils import Const
+
+app_params = dict(source="", target="")
+const = Const()
+const.APP_PARAMS_SOURCE_FILE = "source"
+const.APP_PARAMS_TARGET_FILE = "target"
+
+def load_workbook(file_path):
+    if file_path == None:
+        return None
+    if (os.path.exists(file_path)):
+        return openpyxl.load_workbook(file_path)
+    else:
+        return None
+
+
+def main():
+    try:
+        opts, args = getopt(sys.argv[1], "s:t:", [const.APP_PARAMS_SOURCE_FILE, const.APP_PARAMS_TARGET_FILE])
+    except GetoptError as opt_error:
+        print(opt_error)
+        exit(2)
+    
+
+    
+
+
+
+
