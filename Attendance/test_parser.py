b70dc0f5545a468762d601d796ba538c4292ddf9
goalfinance <theon_pan@outlook.com>
2019-08-08 10:43:11 +0800

    add unit testing codes

--- Attendance/test_parser.py
+++ Attendance/test_parser.py
@@ -4,7 +4,9 @@ from excel.parser import *
 class TestParser(unittest.TestCase):
     """Test parser.py"""
     def test_parse_source_file(self):
-        pass
+        source_workbook = load_workbook(".source_attendance.xlsx")
+        if source_workbook != None and does_monsheet_exist(source_workbook, 7):
+            pass
 
 
 if __name__ == "__main__":
