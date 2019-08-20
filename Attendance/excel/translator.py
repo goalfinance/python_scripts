864d9b6f58d148212ea9952d4a651406b8928dd0
goalfinance <theon_pan@outlook.com>
2019-08-20 15:59:59 +0800

    add codes for generating target attendance records

--- Attendance/excel/translator.py
+++ Attendance/excel/translator.py
@@ -1,6 +1,6 @@
 import openpyxl
 import os
-from goalfinance.utils.calendar import month_to_str
+from goalfinance.utils.calendar import (month_to_str, get_calendar_of_month)
 import numpy as np
 import re
 from calendar import monthrange
@@ -107,3 +107,42 @@ def get_attendances_matrix(source_workbook, year, month):
             attendances_matrix_per_member_compressed[i] = np.max(attendances_matrix_per_member_transposed[i])
             
     return attendances_matrix_compressed, members_full_name
+
+def create_table_header(worksheet, year, month):
+    calendar_of_month = get_calendar_of_month(year, month)
+    headers = [table_title_const.EXCEL_TABLE_HEADER_MEMBER_NAME, table_title_const.EXCEL_TABLE_HEADER_ATTENDANCE_DAYS]
+    for calendar_info in calendar_of_month:
+        headers.append(calendar_info[1])
+    
+    worksheet.append(headers)
+
+def fill_attendances_matrix_to_target_worksheet(worksheet, attendances_matrix, members_full_name, year, month):
+    calendar_of_month = get_calendar_of_month(year, month)
+    for member_id in list(attendances_matrix):
+        member_full_name = members_full_name[member_id]
+        
+        attendance_matrix = attendances_matrix[member_id]
+        if attendance_matrix != None and np.size(attendance_matrix) == len(calendar_of_month):
+            if member_full_name == None:
+                member_full_name = member_id
+            attendance_record = [member_full_name, 0]
+
+            for calendar_info in calendar_of_month:
+                attendance_value = attendance_matrix[calendar_info[1] - 1]
+                if attendance_value == 100:
+                    attendance_record.append(0)
+                elif attendance_value == 50:
+                    attendance_record.append(0.5)
+                else:
+                    attendance_record.append(1)
+
+            worksheet.append(attendance_record)
+
+            #put the formula for calculating the attendance of the month to the cell
+            first_day_cell_coordinate = worksheet.cell(row=worksheet._current_row, column=3).coordinate
+            last_day_cel_coordinate = worksheet.cell(row=worksheet._current_row, column=worksheet.max_column).coordinate
+            attendance_of_month_cell = worksheet.cell(row=worksheet._current_row, column=2)
+            formula_for_attendance = "=SUM(" + first_day_cell_coordinate + ":" + last_day_cel_coordinate + ")"
+            attendance_of_month_cell.value = formula_for_attendance
+
+
